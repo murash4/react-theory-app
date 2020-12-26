7d21a998452f760c2454cf65e56b24c72789af0d
murash4 <qoc306a>
2020-12-26 12:16:58 +0300

    what is JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+	render () {
+		return (
+			<div className="App">
+				<h1>Hellow world</h1>
+			</div>
+			// React.createElement(
+			// 	'div',
+			// 	{
+			// 		className: 'App'
+			// 	},
+			// 	React.createElement(
+			// 		'h1',
+			// 		null,
+			// 		'Hellow world'
+			// 	)
+			// )
+		)
+	}
 }
 
 export default App;
