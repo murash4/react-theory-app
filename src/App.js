b4580deca6a5bcaef5f34f84b2357c3824589e11
murash4 <qoc306a>
2020-12-26 16:54:40 +0300

    work with a list

--- src/App.js
+++ src/App.js
@@ -38,28 +38,22 @@ class App extends Component {
 			textAlign: 'center'
 		}
 
-		const cars = this.state.cars
-
 		return (
 			<div style={ divStyle }>
 				<h1>{ this.state.pageTitle }</h1>
 				<input type="text" onChange={ this.handleInput } />
 				<button onClick={ this.changeTitleHandler.bind(this, 'Changed') }>Change title</button>
-				<Car
-					name={ cars[0].name }
-					year={ cars[0].year }
-					onChangeTitle={ this.changeTitleHandler.bind(this, cars[0].name) }
-				/>
-				<Car
-					name={ cars[1].name }
-					year={ cars[1].year }
-					onChangeTitle={ () => this.changeTitleHandler(cars[1].name) }
-				/>
-				<Car
-					name={ cars[2].name }
-					year={ cars[2].year }
-					onChangeTitle={ () => this.changeTitleHandler(cars[2].name) }
-				/>
+
+				{
+					this.state.cars.map((car, key) => (
+						<Car
+							key={ key }
+							name={ car.name }
+							year={ car.year }
+							onChangeTitle={ this.changeTitleHandler.bind(this, car.name) }
+						/>
+					))
+				}
 			</div>
 		)
 	}
