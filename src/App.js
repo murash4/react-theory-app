7f10509f48d6617633b8d1f64f2f31939361b1c0
murash4 <qoc306a>
2020-12-26 12:48:16 +0300

    add inline-styles in JSX

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import './App.css'
 
 class App extends Component {
 	render () {
+		const divStyle = {
+			textAlign: 'center'
+		}
+
 		return (
-			<div className="App">
-				<h1>Hellow world</h1>
+			<div style={ divStyle }>
+				<h1
+					style={{
+						color: 'blue',
+						fontSize: '30px'
+					}}
+				>Hellow world</h1>
 			</div>
-			// React.createElement(
-			// 	'div',
-			// 	{
-			// 		className: 'App'
-			// 	},
-			// 	React.createElement(
-			// 		'h1',
-			// 		null,
-			// 		'Hellow world'
-			// 	)
-			// )
 		)
 	}
 }
