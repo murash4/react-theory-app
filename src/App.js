52ce71a3b5929192f25a9d1c2e2257ad528accca
murash4 <qoc306a>
2020-12-26 15:23:09 +0300

    passing parameters to a function

--- src/App.js
+++ src/App.js
@@ -21,17 +21,13 @@ class App extends Component {
 		pageTitle: 'React components'
 	}
 
-	changeTitleHandler = () => {
-		const oldTitle = this.state.pageTitle
-		const newTitle = oldTitle + ' changed'
-
+	changeTitleHandler = (newTitle) => {
 		this.setState({
 			pageTitle: newTitle
 		})
 	}
 
 	render () {
-		console.log(1111)
 		const divStyle = {
 			textAlign: 'center'
 		}
@@ -41,10 +37,22 @@ class App extends Component {
 		return (
 			<div style={ divStyle }>
 				<h1>{ this.state.pageTitle }</h1>
-				<button onClick={ this.changeTitleHandler }>Change title</button>
-				<Car name={ cars[0].name } year={ cars[0].year } />
-				<Car name={ cars[1].name } year={ cars[1].year } />
-				<Car name={ cars[2].name } year={ cars[2].year } />
+				<button onClick={ this.changeTitleHandler.bind(this, 'Changed') }>Change title</button>
+				<Car
+					name={ cars[0].name }
+					year={ cars[0].year }
+					onChangeTitle={ this.changeTitleHandler.bind(this, cars[0].name) }
+				/>
+				<Car
+					name={ cars[1].name }
+					year={ cars[1].year }
+					onChangeTitle={ () => this.changeTitleHandler(cars[1].name) }
+				/>
+				<Car
+					name={ cars[2].name }
+					year={ cars[2].year }
+					onChangeTitle={ () => this.changeTitleHandler(cars[2].name) }
+				/>
 			</div>
 		)
 	}
