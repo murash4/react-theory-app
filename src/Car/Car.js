98dbb263343496d953b0394c4e7681346b3fc5eb
murash4 <qoc306a>
2020-12-27 03:01:31 +0300

    add dynamic classes

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,15 +1,30 @@
+import './Car.css'
+
 // eslint-disable-next-line
-export default props => (
-	<div style={{
-		padding: '10px',
-		marginBottom: '10px',
-		border: '1px solid #ccc',
-		boxShadow: '0 4px 5px 0 rgba(0, 0, 0, 0.14)',
-		borderRadius: '5px'
-	}}>
-		<h3>Car name: { props.name }</h3>
-		<p>Year: <strong>{ props.year }</strong></p>
-		<input type="text" onChange={ props.onChangeName } value={ props.name } />
-		<button onClick={ props.onDelete }>Delete</button>
-	</div>
-)
+export default props => {
+	const inputClasses = ['input']
+
+	if (props.name) {
+		inputClasses.push('green')
+	} else {
+		inputClasses.push('red')
+	}
+
+	if (props.name.length > 4) {
+		inputClasses.push('bold')
+	}
+
+	return (
+		<div className="car">
+			<h3>Car name: { props.name }</h3>
+			<p>Year: <strong>{ props.year }</strong></p>
+			<input
+				type="text"
+				onChange={ props.onChangeName }
+				value={ props.name }
+				className={ inputClasses.join(' ') }
+			/>
+			<button onClick={ props.onDelete }>Delete</button>
+		</div>
+	)
+}
