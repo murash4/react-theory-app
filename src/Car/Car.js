1db8230743eb46012169a18f0e0606312d318c99
murash4 <singleant@gmail.com>
2020-12-27 22:40:33 +0300

    Creating a Stateful component

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,45 +1,48 @@
+import React from 'react'
 import './Car.scss'
 import Radium from 'radium'
 
-const Car = props => {
-	const inputClasses = ['input']
+class Car extends React.Component {
+	render () {
+		const inputClasses = ['input']
 
-	if (props.name) {
-		inputClasses.push('green')
-	} else {
-		inputClasses.push('red')
-	}
-
-	if (props.name.length > 4) {
-		inputClasses.push('bold')
-	}
-
-	const style = {
-		border: '1px solid #ccc',
-		boxShadow: '0 4px 5px 0 rgba(0, 0, 0, 0.14)',
-		':hover': {
-			border: '1px solid #aaa',
-			boxShadow: '0 4px 15px 0 rgba(0, 0, 0, 0.25)',
-			cursor: 'pointer'
+		if (this.props.name) {
+			inputClasses.push('green')
+		} else {
+			inputClasses.push('red')
 		}
-	}
 
-	return (
-		<div
-			className="car"
-			style={ style }
-		>
-			<h3>Car name: { props.name }</h3>
-			<p>Year: <strong>{ props.year }</strong></p>
-			<input
-				type="text"
-				onChange={ props.onChangeName }
-				value={ props.name }
-				className={ inputClasses.join(' ') }
-			/>
-			<button onClick={ props.onDelete }>Delete</button>
-		</div>
-	)
+		if (this.props.name.length > 4) {
+			inputClasses.push('bold')
+		}
+
+		const style = {
+			border: '1px solid #ccc',
+			boxShadow: '0 4px 5px 0 rgba(0, 0, 0, 0.14)',
+			':hover': {
+				border: '1px solid #aaa',
+				boxShadow: '0 4px 15px 0 rgba(0, 0, 0, 0.25)',
+				cursor: 'pointer'
+			}
+		}
+
+		return (
+			<div
+				className="car"
+				style={ style }
+			>
+				<h3>Car name: { this.props.name }</h3>
+				<p>Year: <strong>{ this.props.year }</strong></p>
+				<input
+					type="text"
+					onChange={ this.props.onChangeName }
+					value={ this.props.name }
+					className={ inputClasses.join(' ') }
+				/>
+				<button onClick={ this.props.onDelete }>Delete</button>
+			</div>
+		)
+	}
 }
 
 export default Radium(Car)
