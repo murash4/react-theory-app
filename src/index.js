190b467305e9131ea512772f50a1d65c6f7d348c
murash4 <singleant@gmail.com>
2021-01-03 12:24:46 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './redux/rootReducer'
+
+const store = createStore(rootReducer)
 
 const application = (
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
 
