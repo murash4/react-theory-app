c839522c2142e52d739d203334b6a6479821234f
murash4 <singleant@gmail.com>
2021-01-03 13:37:44 +0300

    connect store to component

--- src/CounterNew/CounterNew.js
+++ src/CounterNew/CounterNew.js
@@ -1,27 +1,37 @@
 import { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class CounterNew extends Component {
-	state = {
-		counter: 0
-	}
-
-	updateCounter (value) {
-		this.setState({
-			counter: this.state.counter + value
-		})
-	}
-
+class CounterNew extends Component {
 	render () {
 		return (
 			<div className="CounterNew">
-				<h1>Счетчик <strong>{ this.state.counter }</strong></h1>
+				<h1>Счетчик <strong>{ this.props.counter }</strong></h1>
 				<hr />
 
 				<div className="CounterNew__actions">
-					<button onClick={ () => this.updateCounter(1) }>Добавить 1</button>
-					<button onClick={ () => this.updateCounter(-1) }>Вычесть 1</button>
+					<button onClick={ this.props.onAdd }>Добавить 1</button>
+					<button onClick={ this.props.onSub }>Вычесть 1</button>
 				</div>
 			</div>
 		)
 	}
 }
+
+function mapStateToProps (state) {
+	return {
+		counter: state.counter
+	}
+}
+
+function mapDispatchToProps (dispatch) {
+	return {
+		onAdd: () => dispatch({
+			type: 'ADD'
+		}),
+		onSub: () => dispatch({
+			type: 'SUB'
+		})
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterNew)
